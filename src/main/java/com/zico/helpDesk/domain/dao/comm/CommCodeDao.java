fac265fdc285e42d0d9a2bbc898c0cd133ac9101
zico <zico@192.168.0.4>
2017-04-12 23:23:12 +0900

    zz

--- src/main/java/com/zico/helpDesk/domain/dao/comm/CommCodeDao.java
+++ src/main/java/com/zico/helpDesk/domain/dao/comm/CommCodeDao.java
@@ -5,6 +5,6 @@ import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
 import com.zico.helpDesk.domain.model.entity.comm.CommCode;
 import com.zico.helpDesk.domain.model.entity.comm.embedded.CommCodeId;
-
+// test
 public interface CommCodeDao extends JpaRepository<CommCode, CommCodeId>, QueryDslPredicateExecutor<CommCode> {
 }
