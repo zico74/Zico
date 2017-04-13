5c21898e344d94221b641c8d8e4bce780a02826d
zico <zico@zico-PC>
2017-04-13 15:22:56 +0900

    d

---
+++ src/main/java/com/zico/helpDesk/domain/repository/queryDsl/CommCodeRepositoryQueryDsl.java
@@ -0,0 +1,10 @@
+package com.zico.helpDesk.domain.repository.queryDsl;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.zico.helpDesk.domain.model.entity.comm.CommCode;
+
+public interface CommCodeRepositoryQueryDsl {
+	Page<CommCode> findByGroupCodeIdInfo(Pageable pageable);
+}
